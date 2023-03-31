fc82ebe2e56ddaca11e23fdcce7cc90dbf44bb11
mnhock <Martin.Hock@gfk.com>
2023-03-31 13:05:38 +0200

    Clean up

--- src/test/java/io/naikan/model/architecture/ArchitectureTest.java
+++ src/test/java/io/naikan/model/architecture/ArchitectureTest.java
@@ -5,11 +5,12 @@ import java.util.Collection;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
+import io.naikan.test.architecture.ArchUnitTestsFactory;
+
 class ArchitectureTest {
 
     @TestFactory
     Collection<DynamicTest> testArchitecture() {
-        return ArchUnitTestConfig.builder()
-                .build();
+        return ArchUnitTestsFactory.createTests();
     }
 }
