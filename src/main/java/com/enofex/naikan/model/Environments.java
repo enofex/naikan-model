1b0800a8e1ca46b14f49f090f42ae93a1b7eb19c
mnhock <martin.hock.de@gmail.com>
2023-07-22 16:01:13 +0200

    Fix parameter name

--- src/main/java/com/enofex/naikan/model/Environments.java
+++ src/main/java/com/enofex/naikan/model/Environments.java
@@ -6,8 +6,8 @@ public final class Environments extends AbstractContainer<Environment> {
 
   private static final Environments NO_ENVIRONMENTS = new Environments(List.of());
 
-  public Environments(List<Environment> integrations) {
-    super(integrations);
+  public Environments(List<Environment> environments) {
+    super(environments);
   }
 
   public static Environments empty() {
