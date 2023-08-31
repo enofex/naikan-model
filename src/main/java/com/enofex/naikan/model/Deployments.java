1f9bf5e0cbb790ad15ea544296237fca304439ed
mnhock <martin.hock.de@gmail.com>
2023-08-31 18:53:58 +0200

    Deployments should be sorted

--- src/main/java/com/enofex/naikan/model/Deployments.java
+++ src/main/java/com/enofex/naikan/model/Deployments.java
@@ -1,5 +1,6 @@
 package com.enofex.naikan.model;
 
+import java.util.Comparator;
 import java.util.List;
 
 public final class Deployments extends AbstractContainer<Deployment> {
@@ -7,7 +8,9 @@ public final class Deployments extends AbstractContainer<Deployment> {
   private static final Deployments NO_DEPLOYMENTS = new Deployments(List.of());
 
   public Deployments(List<Deployment> deployments) {
-    super(deployments);
+    super(deployments != null
+        ? deployments.stream().sorted(Comparator.comparing(Deployment::timestamp)).toList()
+        : List.of());
   }
 
   public static Deployments empty() {
